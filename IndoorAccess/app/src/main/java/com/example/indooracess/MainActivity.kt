ffc25d34d99d0674972430d6a07b515c27e500a0
Apoorva Avadhana <apoorvaavadhana@gmail.com>
2023-04-29 19:16:29 -0400

    Printing network info on the console

--- IndoorAccess/app/src/main/java/com/example/indooracess/MainActivity.kt
+++ IndoorAccess/app/src/main/java/com/example/indooracess/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.indooracess
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +19,53 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.indooracess.ui.theme.IndoorAcessTheme
+import kotlin.math.log
+
+
+val TAG : String = "WIFI"
+lateinit var ctx: Context
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ctx  = applicationContext
+
+        // [start] Permissions Check
+        permissionsResultLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+                if (result[Manifest.permission.ACCESS_FINE_LOCATION] != null) {
+                    isLocFinePermissionGranted =
+                        true == result[Manifest.permission.ACCESS_FINE_LOCATION]
+                }
+                if (result[Manifest.permission.ACCESS_NETWORK_STATE] != null) {
+                    isNetworkPermissionGranted =
+                        true == result[Manifest.permission.ACCESS_NETWORK_STATE]
+                }
+                if (result[Manifest.permission.ACCESS_WIFI_STATE] != null) {
+                    isWifiPermissionGranted =
+                        true == result[Manifest.permission.ACCESS_WIFI_STATE]
+                }
+            }
+
+        if (ActivityCompat.checkSelfPermission(
+                ctx,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // If all permissions are granted, Proceed
+            getWifiInfo(ctx)
+        }
+        else{
+            requestPermission(applicationContext)
+        }
+// [end] Permissions Check
+
+
+
         setContent {
             IndoorAcessTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,13 +77,39 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        getWifiInfo(this)
     }
+}
+var bssid: String = "";
+var ssid: String = "";
+
+private fun getWifiInfo(context: Context) {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val wifiManager =
+        context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    if (ActivityCompat.checkSelfPermission(
+            ctx,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    ) {
+        // If all permissions are granted, Proceed
+//        wifiManager.scanResults
+        Log.d(TAG, "getWifiInfo: ${wifiManager.scanResults}")
+    }
+    else{
+        requestPermission(ctx)
+    }
+
+    //WifiInfo is depricated and doesnt work
+    //connectionInfo requires Wifi info and hence doesnt work
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name!, $bssid, $ssid",
         modifier = modifier
     )
 }
